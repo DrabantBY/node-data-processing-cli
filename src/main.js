ff2f6998654852057b9f289fdc3f57e390aacd73
DrabantBY <yauheni.basalai@gmail.com>
2026-06-04 00:25:02 +0300

    feat: implement navigation

---
+++ src/main.js
@@ -0,0 +1,57 @@
+import { homedir } from "node:os";
+import { stdin, stdout } from "node:process";
+import { createInterface } from "node:readline/promises";
+import { parseArgs } from "node:util";
+import { navigateBack, navigateTo, showFileList } from "./navigation.js";
+import { showCurrentDir } from "./repl.js";
+
+const rl = createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+rl.on("close", () => console.log("\nThank you for using Data Processing CLI!"));
+rl.on("SIGINT", () => {
+  rl.close();
+  process.exit(0);
+});
+
+process.chdir(homedir());
+console.log("Welcome to Data Processing CLI!");
+showCurrentDir();
+
+while (true) {
+  const line = await rl.question("> ");
+  const { positionals, values } = parseArgs({
+    args: line.trim().split(/\s+/),
+    allowPositionals: true,
+  });
+
+  if (
+    positionals?.length === 1 &&
+    positionals[0] === "up" &&
+    Object.keys(values).length === 0
+  ) {
+    navigateBack();
+  }
+
+  if (
+    positionals?.length === 1 &&
+    positionals[0] === "ls" &&
+    Object.keys(values).length === 0
+  ) {
+    await showFileList();
+  }
+
+  if (
+    positionals?.length === 2 &&
+    positionals[0] === "cd" &&
+    Object.keys(values).length === 0
+  ) {
+    navigateTo(positionals[1]);
+  }
+
+  showCurrentDir();
+
+  console.log("positionals", positionals, "values", values);
+}
---
+++ src/navigation.js
@@ -0,0 +1,35 @@
+import { readdir } from "node:fs/promises";
+import { sep } from "node:path";
+
+export const navigateBack = () => {
+  process.chdir("..");
+};
+
+export const navigateTo = (target) => {
+  const isRoot = /^[a-z]:$/i.test(target);
+  process.chdir(`${target}${isRoot ? sep : ""}`);
+};
+
+export const showFileList = async () => {
+  const dir = process.cwd();
+
+  const entries = await readdir(dir, { recursive: true, withFileTypes: true });
+
+  const table = entries
+    .reduce((acc, entry) => {
+      if (entry.isDirectory()) {
+        acc.push({ name: entry.name, type: "directory" });
+      }
+
+      if (entry.isFile()) {
+        acc.push({ name: entry.name, type: "file" });
+      }
+
+      return acc;
+    }, [])
+    .sort(
+      (a, b) => a.type.localeCompare(b.type) || a.name.localeCompare(b.name),
+    );
+
+  console.table(table);
+};
